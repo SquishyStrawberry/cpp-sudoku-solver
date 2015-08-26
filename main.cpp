314a741b7773dd8fcc80b82e595b1bd370ef49db
SquishyStrawberry <therealrebuilder@gmail.com>
2015-08-26 18:05:53 +0200

    Added getline-ing the board

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 int main() {
-    string board = "000105000140000670080002400063070010900000003010090520007200080026000035000409000";
+    string board;
+    getline(cin, board);
     Sudoku game = board;
     game.print();
     game.solve();
